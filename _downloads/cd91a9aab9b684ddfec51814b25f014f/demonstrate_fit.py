45b6453973fc88cff2e13aa7bed3332ca1b84d5f
matthew-balogh <matthew-balogh@users.noreply.github.com>
2026-07-12 15:20:18 +0000

    [ci skip] Update examples 15c928f1a10b3b246bc99110bc7396f0bddf966e

--- _downloads/cd91a9aab9b684ddfec51814b25f014f/demonstrate_fit.py
+++ _downloads/cd91a9aab9b684ddfec51814b25f014f/demonstrate_fit.py
@@ -23,3 +23,20 @@ X_test, y_test = generate_train_data(n_normal=100, n_abnormal=20, random_seed=84
 from sklearn.preprocessing import StandardScaler
 estimator = ParallelAnomalousNudge(scaler=StandardScaler(), nu=.05, omega=2.0, random_seed=42, verbose=True)
 estimator.fit(X_train, y_train)
+
+# %%
+# The `ParallelAnomalousNudge` itself does not support setting the hyperparameters for the class-conditioned SOM representations that are being learned internally.
+# Instead, one can pass blueprints of :class:`pan.pan.SomRepresentation` as parameters that can be configured for each class.
+
+from pan import ParallelAnomalousNudge, SomRepresentation
+
+som_estimator_normal = SomRepresentation(d1=10, d2=5, sigma=5.0, learning_rate=.6, num_iteration=5, verbose=True)
+som_estimator_abnormal = SomRepresentation(d1=3, d2=3, sigma=2.5, learning_rate=.6, num_iteration=5, verbose=True)
+
+som_estimators = {
+    0: som_estimator_normal,
+    1: som_estimator_abnormal
+}
+
+estimator = ParallelAnomalousNudge(estimators=som_estimators, scaler=StandardScaler(), nu=.05, omega=2.0, random_seed=42, verbose=True)
+estimator.fit(X_train, y_train)
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev28+gd7755ff44',
+    VERSION: '0.1.dev30+g15c928f1a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
